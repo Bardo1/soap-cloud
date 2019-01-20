95df0d6fabc8533708ecaeae7dd62c452dbe684e
wzxuse <1518776497@qq.com>
2019-01-20 14:13:32 +0800

    update systen service

--- soap-modules/system-service/src/main/java/org/wzx/cloud/controller/SystemController.java
+++ soap-modules/system-service/src/main/java/org/wzx/cloud/controller/SystemController.java
@@ -1,36 +1,13 @@
 package org.wzx.cloud.controller;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.wzx.cloud.model.entity.MenuDO;
-import org.wzx.cloud.model.vo.R;
-import org.wzx.cloud.service.MenuService;
 
-import java.util.List;
-
+@Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("sysconst")
 public class SystemController {
 
-    @Autowired
-    private MenuService menuService;
 
-    @GetMapping("/list")
-    public R listMenu(@RequestParam("parentid") String parentid) {
-        if (StringUtils.isBlank(parentid)) {
-            R.error("传入的parentid不能为空");
-        }
-        try {
-            List<MenuDO> list = menuService.listMenu(parentid);
-            return R.ok(list);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return R.error(e.getMessage());
-        }
-
-    }
 }
